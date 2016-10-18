9c30b1e1c4dd26ac81202cf17a8ed6ff23e40c88
Steffen Exler <Steffen.Exler@gmail.com>
2016-10-18 17:14:02 +0200

    update

--- src/main/java/PolynomialForm.java
+++ src/main/java/PolynomialForm.java
@@ -6,6 +6,16 @@ import javax.swing.*;
 public class PolynomialForm {
 
     private JPanel MainPanel;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
 
     public static void main() {
         JFrame frame = new JFrame("PolyForm");
