c1b2b4fa61ea95de6bf9b0fd9dc677099e58aaf2
Steffen Exler <steffen.exler@gmail.com>
2016-11-01 10:10:59 +0100

    update

---
+++ src/test/java/PolynomialTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by fubu on 01.11.16.
+ */
+public class PolynomialTest {
+    @org.junit.Test
+    public void get() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void get1() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void set() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void set1() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void length() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void derivation() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void get_as_human_readable() throws Exception {
+
+    }
+
+}
